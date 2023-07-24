44701a4c667815d16ab29200e64d4eb6ffc238f1
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-07-24 00:13:59 -0500

    progress

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 $(document).ready(handleReady);
 
-let input;
+let inputOperation;
+let solution;
 let plus = false;
 let minus = false;
 let multiply = false;
@@ -10,32 +11,40 @@ function handleReady() {
     console.log("jquery is loaded!")
 
     $('button').on("click", selectOperator);
-
-    $('#plus').on("click", handleSubmit);
+    $('#equals').on("click", handleSubmit);
 
 }
 
 
-
+// Submit and send data
 function handleSubmit() {
 
-let input1 = $('#input1').val();
-let input2 = $('#input2').val();
-// let operation = ;
+    let input1 = $('#input1').val();
+    let input2 = $('#input2').val();
+    inputOperation = {
+        val1: input1,
+        val2: input2,
+        plus: plus,
+        minus: minus,
+        multiply: multiply,
+        divide: divide
+    };
 
     $.ajax({
         method: "POST",
-        url: '/input',
-        data: input
-      }).then((response) => {
+        url: '/calculations',
+        data: inputOperation
+    }).then((response) => {
+        handleData()
         console.log("POST was successful:", response)
-      }).catch((error) => {
+        console.log("POST was:", inputOperation)
+    }).catch((error) => {
         // console.log('error caught', error)
         // alert('ERRORRRRR')
-      })
+    })
 }
 
-
+// Determine operator to use
 function selectOperator() {
     if ($(this).hasClass('operationOff')) {
         $(this).removeClass('operationOff')
@@ -65,4 +74,23 @@ function selectOperator() {
         divide = false;
     }
     // console.log(plus, minus, multiply, divide);
+}
+
+function handleData() {
+    $.ajax({
+        method: "GET",
+        url: '/solution',
+    }).then((response) => {
+        solution = response
+        console.log("GET was successful:", solution)
+        render()
+    }).catch((error) => {
+        // console.log('error caught', error)
+        // alert('ERRORRRRR')
+    })
+}
+
+
+function render() {
+    $('#solution').text(`${solution}`)
 }
--- public/server.js
+++ public/server.js
@@ -2,11 +2,72 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
+let recievedData;
+let solution;
+let operationHistory = [];
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('server/public'));
+app.use(express.static('public'));
 
 
+app.post('/calculations', (req, res) => {
+  console.log('made it to req body', req.body)
+  recievedData = req.body
+  calculations(recievedData)
+  res.sendStatus(201)
+})
+
+app.get('/solution', (req, res) => {
+  res.send(solution + '')
+  console.log('solution is', solution);
+})
+
+function calculations(data) {
+  
+  if (data.plus == true) {
+    solution = data.val1 + data.val2
+    operationHistory.unshift(
+      {
+        input1: input1,
+        input2: input2,
+        operation: plus
+      }
+    )
+  }
+
+  else if (recievedData.minus == true) {
+    solution = Number((recievedData.input1) - (recievedData.input2))
+    operationHistory.unshift(
+      {
+        input1: input1,
+        input2: input2,
+        operation: minus
+      }
+    )
+  }
+
+  else if (recievedData.multiply == true) {
+    solution = Number((recievedData.input1) * (recievedData.input2))
+    operationHistory.unshift(
+      {
+        input1: input1,
+        input2: input2,
+        operation: multiply
+      }
+    )
+  }
+
+  else if (recievedData.divide == true) {
+    solution = Number((recievedData.input1) / (recievedData.input2))
+    operationHistory.unshift(
+      {
+        input1: input1,
+        input2: input2,
+        operation: divide
+      }
+    )
+  }
+}
 
 
 
